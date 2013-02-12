ae84116a6281b835def25ee34c7f90a4b5ce5130
David Johansson <davidjohansson@outlook.com>
2013-02-12 14:25:50 +0100

    Version 0.01

--- The_Smart_Power_Strip/The_Smart_Power_Strip.ino
+++ The_Smart_Power_Strip/The_Smart_Power_Strip.ino
@@ -1,3 +1,5 @@
+
+
 /*
  The smart power strip,
  
@@ -15,8 +17,12 @@ const int digitalLayout[] = {2,3,4,5,6,7,8,9,10,11,12,13};          //Array for 
 
 
 // Global Variables -----------------------------------------------
+const int analogLayoutLength = 6; // The length of the analogLayout vector.
+const int digitalLayoutLength = 12; // The length of the digitalLayout vector.
 int sensorValue = 0;        // value read from the pot  - From example
 int outputValue = 0;        // value output to the PWM (analog out) - From example
+int analogReadState[analogLayoutLength]; // Stores the read state of the analog pins
+int digitalReadState[digitalLayoutLength]; // Stores the read state of the digital pins
 // Global Variables ------------------------------------------------
 
 
@@ -30,24 +36,49 @@ void setup() {
 
 void loop() {
   
-  //TEsalkjghsalkjdfhaksldjhflaksdjmfha
   
+  // Read the values from the analog and digital pins---------------
+  for (int x = 1; x <= analogLayoutLength; x++){    // Read the analog pins and store them in analogReadstate
+    analogReadState[x] = analogRead(analogLayout[x]);
+  }
+
+  for (int x = 1; x <= digitalLayoutLength; x++){  // Read the digital pins and store them in digitalReadstate
+    digitalReadState[x] = digitalRead(digitalLayout[x]);
+  }
+  // Read the values from the analog and digital pins---------------
+
+ 
   
-  // read the analog in value:
-  sensorValue = analogRead(analogInPin);            
-  // map it to the range of the analog out:
-  outputValue = map(sensorValue, 0, 1023, 0, 255);  
-  // change the analog out value:
-  analogWrite(analogOutPin, outputValue);           
+  // Print the values through the serial port ----------------------
+  // Analog Values --------------
+  Serial.println("Value on A0: "); 
+  Serial.print(analogReadState[0]);
+  
+  Serial.println("Value on A1: "); 
+  Serial.print(analogReadState[1]);
+  
+  Serial.println("Value on A2: "); 
+  Serial.print(analogReadState[2]);
+  
+  Serial.println("Value on A3: "); 
+  Serial.print(analogReadState[3]);
+  
+  Serial.println("Value on A4: "); 
+  Serial.print(analogReadState[4]);
+  
+  Serial.println("Value on A5: "); 
+  Serial.print(analogReadState[5]);
+  // Analog Values --------------
+  // Print the values through the serial port ----------------------
+}
 
-  // print the results to the serial monitor:
-  Serial.print("sensor = " );                       
-  Serial.print(sensorValue);      
-  Serial.print("\t output = ");      
-  Serial.println(outputValue);   
-
-  // wait 2 milliseconds before the next loop
-  // for the analog-to-digital converter to settle
-  // after the last reading:
-  delay(2);                     
+float powerCalc (int current[], int voltage[]) {
+  
+ float power = 10;
+ int n= 10;
+ for (int i=0; i <= n-1; i++){
+   
+ }
+ return power;
+  
 }
