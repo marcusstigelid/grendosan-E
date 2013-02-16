9bb499ae49e5fb5c7d8c049e7d4f357a22d25b5a
Philip Karlsson <Philipkarlsson@me.com>
2013-02-16 20:07:29 +0100

    Version 0.06

--- The_Smart_Power_Strip/The_Smart_Power_Strip.ino
+++ The_Smart_Power_Strip/The_Smart_Power_Strip.ino
@@ -45,6 +45,8 @@ String control_message = "id:auth:1;0;1;1";     //The controll message string re
 String ID = "testID";
 int counter = 0;
 boolean bufferIsEmpty = true;
+int phaseDiff[analogLayoutLength];
+int sampleRate; // Samples/sek
 // Global Variables ------------------------------------------------
 
 
@@ -258,7 +260,56 @@ boolean send_update () {
 // Try to send data to server -------------------------------------
 
 
+// Fuction for finding zero in an array ---------------------------
+int findZero(apvector<int> &array){
+  int lastValue = array[0];
+  for (int i=0; i<sampleNumber; i++){
+    if((lastValue > 0 && array[i] < 0) || (lastValue < 0 && array[i] > 0) || array[i] = 0){
+      return i;
+    }
+  }
+}
+// Fuction for finding zero in an array ---------------------------
+
+// Gör om funktionen nedan......
+//Function to find highest (maximum) value in array ---------------
+int maximumValue(apvector<int> &array)
+{    
+     int length = array.length( );  // establish size of array
+     int max = array[i];       // start with max = first element
+     for(int i = (i); i<length; i++)
+     {
+          if(array[i] > max)
+                max = array{i};
+     }
+     return max;                // return highest value in array
+}
+
+//Function to find highest (maximum) value in array ---------------
+
+// Phase Differnece -----------------------------------------------
+void phaseDifference(){
+  int highestVoltageSampleNumber; //Tells on which sample the highest value of the voltage is located
+  int local_vector[sampleNumber]; //Local vector used for calculations
+  for(int y=0; y<analogLayoutLength; y++){
+    for(int x=0; x<sampleNumber; x++){
+      local_vector[x] = analogReadState[y][x]; // Store in the local variable
+    }
+    if(y=0){
+      highestVoltageSampleNumber = maximumValue(local_vector); // Load the highest voltage sample
+    }
+    if(y!=0){
+       int sampleDiff =  maximumValue(local_vector) - highestVoltageSampleNumber; //(May be positive or negative)
+       phaseDiff[y] = (((sampleDiff)*sampleRate)/0.02)*360;  //Algorithm for converting samples to degrees
+    }
+  }
+}
+// Phase Difference -----------------------------------------------
 
 
+
+
+
+// Sätt ett bestämt värde där man vet en spännigstopp!! utgå ifrån det när du hittar toppen för strömmen....
 
 
