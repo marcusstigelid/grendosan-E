088108111cae9c6e7115dbc3ff7d32b2951591d3
David Johansson <davidjohansson@outlook.com>
2013-03-07 14:35:00 +0100

    Version 1.23

--- The_Smart_Power_Strip/Calculations.ino
+++ The_Smart_Power_Strip/Calculations.ino
@@ -34,52 +34,52 @@ void powerCalc () {
   }
 }
 
-/*
-// Fuction for finding zero in an array ---------------------------
-int findZero(apvector<int> &array){
-  int lastValue = array[0];
-  for (int i=0; i<sampleNumber; i++){
-    if((lastValue > 0 && array[i] < 0) || (lastValue < 0 && array[i] > 0) || array[i] = 0){
+
+// Fuction for finding zero in an array --------------------------- (Part of the Phase Differance calculations)
+int findZero(int init, int array[]){
+  int lastValue = array[init];
+  for (int i=init; i<sampleNumber; i++){
+    if(((lastValue > 0) && (array[i] < 0)) || ((lastValue < 0) && (array[i] > 0)) || array[i] == 0){
       return i;
     }
   }
 }
-// Fuction for finding zero in an array ---------------------------
+// Fuction for finding zero in an array --------------------------- (Part of the Phase Differance calculations)
 
 
-// Gör om funktionen nedan......
-//Function to find highest (maximum) value in array ---------------
-int maximumValue(apvector<int> &array)
+//Function to find highest (maximum) value in array --------------- (Part of the Phase Differance calculations)
+int maximumValue(int start, int stop_, int array[])
 {    
-     int length = array.length( );  // establish size of array
-     int max = array[i];       // start with max = first element
-     for(int i = (i); i<length; i++)
+     int max = array[start];       // start with max = first element
+     for(int i = start; i<stop_; i++)
      {
-          if(array[i] > max)
-                max = array{i};
+          if(array[i] > max){
+                max = array[i];
+          }
      }
      return max;                // return highest value in array
 }
 
-//Function to find highest (maximum) value in array ---------------
-
+//Function to find highest (maximum) value in array --------------- (Part of the Phase Differance calculations)
 
 // Phase Differnece -----------------------------------------------
 void phaseDifference(){
   int highestVoltageSampleNumber; //Tells on which sample the highest value of the voltage is located
+  int firstzeroCrossing, secondzeroCrossing;
   int local_vector[sampleNumber]; //Local vector used for calculations
   for(int y=0; y<analogLayoutLength; y++){
     for(int x=0; x<sampleNumber; x++){
       local_vector[x] = analogReadState[y][x]; // Store in the local variable
     }
     if(y=0){
-      highestVoltageSampleNumber = maximumValue(local_vector); // Load the highest voltage sample
+      firstzeroCrossing = findZero(50, local_vector);//Find first zero crossing in the voltage vecor with guidance from the given sample index (50 in this case )
+      secondzeroCrossing = findZero((firstzeroCrossing + 40), local_vector); // the findzeroCrossing(argument 1) should be close to the end of 0----->T as guidance so 40 is probably wrong... 
+      highestVoltageSampleNumber = maximumValue(firstzeroCrossing, secondzeroCrossing, local_vector); // Load the highest voltage sample
     }
     if(y!=0){
-       int sampleDiff =  maximumValue(local_vector) - highestVoltageSampleNumber; //(May be positive or negative)
+       int sampleDiff =  maximumValue(firstzeroCrossing, secondzeroCrossing, local_vector) - highestVoltageSampleNumber; //(May be positive or negative)
        phaseDiff[y] = (((sampleDiff)*sampleRate)/0.02)*360;  //Algorithm for converting samples to degrees
     }
   }
 }
 // Phase Difference -----------------------------------------------
-*/
